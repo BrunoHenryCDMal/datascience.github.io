621640b6c823e28f037cc15b79c4a4425348ab51
Bruno Miguel Barreiro Henriques <BrunoHenriques@MacBook-Pro-2.local>
2018-10-02 13:41:38 +0200

    no message

---
+++ contact.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<!-- The HTML <head> element has nothing to do with HTML headings.
+The <head> element is a container for metadata. HTML metadata is 
+data about the HTML document. Metadata is not displayed.
+The <head> element is placed between the <html> tag and the <body> tag:-->
+<head>
+  <title>W3 course on HTML</title>
+  <meta charset="UTF-8">
+  
+ <link rel="stylesheet" href="new_styles.css">  
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<meta charset="UTF-8">
+<meta name="description" content="Web tutorial">
+<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+<meta name="author" content="Bruno Henriques">
+<meta http-equiv="refresh" content="30">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body style="font-family:lucida grande;color:#aaaaaa;">
+
+
+
+
+<?php include('topnav.php'); ?>
+
+
+
+
+<div class="top_panel">
+	<h1 style="padding:2%;">Predictive Learning</h1>
+	<div id="top_panel_mask1"> </div>
+	<div id="top_panel_mask2"> </div>
+	<div id="top_panel_mask3"> </div>
+	<div id="top_panel_mask4"> </div>
+</div> 
+
+
+ 
+
+
+<div class="bottom_panel">
+            
+	<p style="font-size:3vw;">Data Science Projects by Bruno Henriques<p>
+	<p style="font-size:2vw;">explore data patterns, construct robust models, predict future trends<p>     
+	
+            
+</div>
+
+</body>
+</html>
---
+++ topnav.php
@@ -0,0 +1,1 @@
+<a href="/index.html">Home</a><a href="/game">Games</a>;
